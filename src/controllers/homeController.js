ff86d4142b010ee0c74cd38bd6cf7b31f8fdb98a
isam chajia <isamchajiabeat@gmail.com>
2025-09-30 16:32:01 +0100

    fix ejs

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,5 +1,7 @@
 export const home = (req, res) => {
-  return res.render('pages/index', { title: 'Home', message: `Welcome, ${req.user?.name || 'Guest'}!` });
+  const userName = req.user?.name || 'Guest';
+  const emailVerified = Boolean(req.session && req.session.emailVerified);
+  return res.render('pages/index', { title: 'Home', userName, emailVerified });
 };
 
 export const about = (req, res) => {
--- src/controllers/profileController.js
+++ src/controllers/profileController.js
@@ -1,7 +1,12 @@
 export const profile = (req, res) => {
   const user = req.user;
+  const emailVerified = Boolean(req.session && req.session.emailVerified);
+
+  console.log(emailVerified);
+  console.log(user);
   return res.render('pages/profile', {
     title: 'Profile page',
-    user
+    user,
+    emailVerified
   });
 }; 
